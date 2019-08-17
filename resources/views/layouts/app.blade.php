1e9c380fe5dfbdfdb92c582651a841ce0499d221
artisanlabs <artisan254@gmail.com>
2019-08-17 14:19:31 +0300

    Created the user layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,75 +10,145 @@
 	<title>{{ config('app.name', 'Laravel') }}</title>
 	
 	<!-- Scripts -->
-	<script src="{{ asset('js/app.js') }}" defer></script>
-	
-	<!-- Fonts -->
-	<link rel="dns-prefetch" href="//fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+	<script src="{{ asset('js/app.js') }}"></script>
 	
 	<!-- Styles -->
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-<div id="app">
-	<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-		<div class="container">
-			<a class="navbar-brand" href="{{ url('/') }}">
-				{{ config('app.name', 'Laravel') }}
-			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-			        aria-controls="navbarSupportedContent" aria-expanded="false"
-			        aria-label="{{ __('Toggle navigation') }}">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<!-- Left Side Of Navbar -->
-				<ul class="navbar-nav mr-auto">
-				
-				</ul>
-				
-				<!-- Right Side Of Navbar -->
-				<ul class="navbar-nav ml-auto">
-					<!-- Authentication Links -->
-					@guest
-						<li class="nav-item">
-							<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-						</li>
-						@if (Route::has('register'))
-							<li class="nav-item">
-								<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-							</li>
-						@endif
-					@else
-						<li class="nav-item dropdown">
-							<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-							   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-								{{ Auth::user()->name }} <span class="caret"></span>
-							</a>
-							
-							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-								<a class="dropdown-item" href="{{ route('logout') }}"
-								   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-									{{ __('Logout') }}
-								</a>
-								
-								<form id="logout-form" action="{{ route('logout') }}" method="POST"
-								      style="display: none;">
-									@csrf
-								</form>
-							</div>
-						</li>
-					@endguest
-				</ul>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper" id="app">
+	<!-- Navbar -->
+	<nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+		<!-- Left navbar links -->
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+			</li>
+			<li class="nav-item d-none d-sm-inline-block">
+				<a href="{{ route('home') }}" class="nav-link">Home</a>
+			</li>
+		
+		</ul>
+		
+		<!-- SEARCH FORM -->
+		<form class="form-inline ml-3">
+			<div class="input-group input-group-sm">
+				<input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+				<div class="input-group-append">
+					<button class="btn btn-navbar" type="submit">
+						<i class="fa fa-search"></i>
+					</button>
+				</div>
 			</div>
-		</div>
-	</nav>
+		</form>
 	
-	<main class="py-4">
+	</nav>
+	<!-- /.navbar -->
+	
+	<!-- Main Sidebar Container -->
+	<aside class="main-sidebar sidebar-dark-primary elevation-4">
+		<!-- Brand Logo -->
+		<a href="{{ route('home') }}" class="brand-link">
+			<img src="{{ asset('images/logo.png') }}" alt="App logo" class="brand-image img-circle elevation-3"
+			     style="opacity: .8">
+			<span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+		</a>
+		
+		<!-- Sidebar -->
+		<div class="sidebar">
+			<!-- Sidebar user panel (optional) -->
+			<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+				<div class="image">
+					<img src="{{ asset('images/avatar.png') }}" class="img-circle elevation-2" alt="User Image">
+				</div>
+				<div class="info">
+					<a href="#" class="d-block">{{ auth()->user()->name }}</a>
+				</div>
+			</div>
+			
+			<!-- Sidebar Menu -->
+			<nav class="mt-2">
+				<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+				    data-accordion="false">
+					<li class="nav-item">
+						<a href="{{ route('home') }}" class="nav-link">
+							<i class="nav-icon fas fa-tachometer-alt"></i>
+							<p>Dashboard</p>
+						</a>
+					</li>
+					<li class="nav-item has-treeview">
+						<a href="#" class="nav-link">
+							<i class="nav-icon fa fa-building"></i>
+							<p>
+								Requests
+								<i class="right fa fa-angle-left"></i>
+							</p>
+						</a>
+						<ul class="nav nav-treeview">
+							<li class="nav-item">
+								<a href="" class="nav-link">
+									<i class="fa fa-plus-circle nav-icon"></i>
+									<p>New Requests</p>
+								</a>
+							</li>
+						</ul>
+					</li>
+					<li class="nav-item has-treeview">
+						<a href="#" class="nav-link">
+							<i class="nav-icon fa fa-user-cog"></i>
+							<p>
+								My Account
+								<i class="right fa fa-angle-left"></i>
+							</p>
+						</a>
+						<ul class="nav nav-treeview">
+							<li class="nav-item">
+								<a href="" class="nav-link">
+									<i class="fa fa-key nav-icon"></i>
+									<p>Password Settings</p>
+								</a>
+							</li>
+							{{--<li class="nav-item">--}}
+							{{--<a href="{{ route('account.profile') }}" class="nav-link">--}}
+							{{--<i class="fa fa-user nav-icon"></i>--}}
+							{{--<p>My Profile</p>--}}
+							{{--</a>--}}
+							{{--</li>--}}
+						</ul>
+					</li>
+					<li class="nav-item">
+						<a href="{{ route('logout') }}" class="nav-link"
+						   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+							<i class="nav-icon fas fa-sign-out-alt"></i>
+							<p>Logout</p>
+							
+							<form id="logout-form" action="{{ route('logout') }}" method="POST"
+							      style="display: none;">
+								@csrf
+							</form>
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<!-- /.sidebar-menu -->
+		</div>
+		<!-- /.sidebar -->
+	</aside>
+	
+	<!-- Content Wrapper. Contains page content -->
+	<div class="content-wrapper">
 		@yield('content')
-	</main>
+	</div>
+	<!-- /.content-wrapper -->
+	<!-- Main Footer -->
+	<footer class="main-footer text-center">
+		<strong>
+			Copyright &copy; {{ date('Y') }} <a href="{{ config('app.url') }}">{{ config('app.name') }}</a>.
+		</strong>
+	</footer>
+	<!-- Scripts -->
+	<script src="{{ asset('js/app.js') }}"></script>
+	@stack('scripts')
 </div>
 </body>
 </html>
