e397987f0dad42720d69862d53d02878fbe8b3b8
artisanlabs <artisan254@gmail.com>
2019-08-17 14:42:13 +0300

    Added the password routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,18 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group([
+	'prefix' => 'account',
+	'as' => 'account.',
+], function () {
+	// Show the admin dashboard
+	Route::get('password', 'SystemController@showPasswordForm')->name('password');
+	Route::put('password', 'SystemController@updatePassword')->name('password');
+});
+
+/**
+ * All the admin routes go here
+ */
+Route::group([
 	'prefix' => 'admin',
 	'as' => 'admin.',
 ], function () {
