be089160de7f6081543cc23081c55a0bc1f1fafd
artisanlabs <artisan254@gmail.com>
2019-08-18 15:59:47 +0300

    Updated the card header

--- resources/views/admin/equipments/create.blade.php
+++ resources/views/admin/equipments/create.blade.php
@@ -7,7 +7,7 @@
 		<div class="container-fluid">
 			<div class="row mb-2">
 				<div class="col-sm-6">
-					<h1>Update Password</h1>
+					<h1>Upload Equipments CSV</h1>
 				</div>
 				<div class="col-sm-6">
 					<div class="breadcrumb float-sm-right">
