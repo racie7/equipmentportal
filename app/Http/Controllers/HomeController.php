c622300b303928e7de786c9865430db3d5d3b984
artisanlabs <artisan254@gmail.com>
2019-08-18 16:00:21 +0300

    Fetched the available equipments

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Equipment;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller {
@@ -23,5 +24,18 @@ class HomeController extends Controller {
 	 */
 	public function index() {
 		return view('home');
+	}
+
+	public function showEquipments() {
+		// Get the available equipment
+		$equipments = Equipment::where('is_available', true)->paginate(500, [
+			'id', 'tag_number', 'description', 'serial_number', 'cost', 'nbv', 'location',
+		]);
+
+		return view('equipments.available', compact('equipments'));
+	}
+
+	public function requestEquipment(Request $request) {
+		return $request;
 	}
 }
