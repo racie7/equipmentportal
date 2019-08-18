5003e7d298afb310324f4d07caed967ab9b7ae97
artisanlabs <artisan254@gmail.com>
2019-08-18 16:00:53 +0300

    Checked if there are any equipments

--- resources/views/admin/equipments/index.blade.php
+++ resources/views/admin/equipments/index.blade.php
@@ -24,62 +24,82 @@
 			<!-- /.row -->
 			<div class="row">
 				<div class="col-12">
-					<div class="card">
-						<div class="card-header">
-							<h3 class="card-title">Available Equipments</h3>
-							
-							<div class="card-tools">
-								<div class="input-group input-group-sm" style="width: 150px;">
-									<input type="text" name="table_search" class="form-control float-right"
-									       placeholder="Search">
-									
-									<div class="input-group-append">
-										<button type="submit" class="btn btn-default"><i class="fa fa-search"></i>
-										</button>
+					@if(count($equipments))
+						<div class="card">
+							<div class="card-header">
+								<h3 class="card-title">Available Equipments</h3>
+								
+								<div class="card-tools">
+									<div class="input-group input-group-sm" style="width: 150px;">
+										<input type="text" name="table_search" class="form-control float-right"
+										       placeholder="Search">
+										
+										<div class="input-group-append">
+											<button type="submit" class="btn btn-default"><i class="fa fa-search"></i>
+											</button>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-						<!-- /.card-header -->
-						<div class="card-body table-responsive p-0">
-							<table class="table table-hover table-bordered">
-								<thead>
-								<tr>
-									<th width="1">#</th>
-									<th>Tag NO</th>
-									<th>Class</th>
-									<th>Description</th>
-									<th>Serial NO</th>
-									<th>Cost</th>
-									<th>NBV</th>
-									<th>Location</th>
-								</tr>
-								</thead>
-								<tbody>
-								@foreach($equipments as $equipment)
-									@php($cost = is_null($equipment->cost) ? 'N/A' : number_format($equipment->cost))
+							<!-- /.card-header -->
+							<div class="card-body table-responsive p-0">
+								<table class="table table-hover table-bordered">
+									<thead>
 									<tr>
-										<td>{{ $loop->iteration }}</td>
-										<td>{{ $equipment->tag_number }}</td>
-										<td>{{ $equipment->class }}</td>
-										<td>{{ $equipment->description }}</td>
-										<td>{{ $equipment->serial_number }}</td>
-										<td>{{ $cost }}</td>
-										<td>{{ number_format($equipment->nbv) }}</td>
-										<td>{{ $equipment->location }}</td>
+										<th width="1">#</th>
+										<th>Tag NO</th>
+										<th>Class</th>
+										<th>Description</th>
+										<th>Serial NO</th>
+										<th>Cost</th>
+										<th>NBV</th>
+										<th>Location</th>
 									</tr>
-								@endforeach
-								</tbody>
-							</table>
+									</thead>
+									<tbody>
+									@foreach($equipments as $equipment)
+										@php($cost = is_null($equipment->cost) ? 'N/A' : number_format($equipment->cost))
+										<tr>
+											<td>{{ $loop->iteration }}</td>
+											<td>{{ $equipment->tag_number }}</td>
+											<td>{{ $equipment->class }}</td>
+											<td>{{ $equipment->description }}</td>
+											<td>{{ $equipment->serial_number }}</td>
+											<td>{{ $cost }}</td>
+											<td>{{ number_format($equipment->nbv) }}</td>
+											<td>{{ $equipment->location }}</td>
+										</tr>
+									@endforeach
+									</tbody>
+								</table>
+							</div>
+							<div class="card-footer clearfix">
+								<ul class="pagination pagination-sm m-0 float-right">
+									{{ $equipments->appends($_GET)->links() }}
+								</ul>
+							</div>
+							<!-- /.card-body -->
 						</div>
-						<div class="card-footer clearfix">
-							<ul class="pagination pagination-sm m-0 float-right">
-								{{ $equipments->appends($_GET)->links() }}
-							</ul>
+					@else
+						<div class="card">
+							<div class="container" style="margin: 40px 0">
+								<div class="row">
+									<div class="col-md-6 offset-md-3">
+										<div class="alert alert-info alert-dismissible text-center">
+											No equipments were found. Please add some.
+										</div>
+									</div>
+								</div>
+								<div class="text-center">
+									<a href="{{ route('admin.equipments.create') }}"
+									   class="btn btn-primary text-center">
+										Add Equipments
+									</a>
+								</div>
+							</div>
 						</div>
-						<!-- /.card-body -->
-					</div>
-					<!-- /.card -->
+				@endif
+				<!-- /.card -->
 				</div>
 			</div><!-- /.row -->
 		</div><!-- /.container-fluid -->
